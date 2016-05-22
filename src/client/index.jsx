329ddf164b04ca5d2de1f1c3a81dca1cfebab5d2
Mitchell Barron <hello@mitchellbarron.io>
2016-05-22 18:40:23 -0500

    go.

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import horizon from './lib/horizon.js';
 
 function App() {
-  return <p>It's working</p>;
+  return <p>Example</p>;
 }
 
 render(<App />, document.getElementById('app'));
